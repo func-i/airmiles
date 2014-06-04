7d430753be8d7d2bca99a378365f805c7dbc66dd
Jonathan Salis <jtsseneca@hotmail.com>
2014-06-03 23:56:37 -0400

    Adding client functionality and apis

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+
+  def index
+    @people = [
+      OpenStruct.new({name: "Jon", weight: 160, height: 69}),
+      OpenStruct.new({name: "Josh", weight: 180, height: 71})
+    ]
+  end
+
   def add
     @answer = params[:left] + params[:right]
     render :index
